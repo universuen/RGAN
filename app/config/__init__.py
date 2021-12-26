83457241987eb17ab98732d87406d2eda2dde560
universuen <52519513+universuen@users.noreply.github.com>
2021-12-26 12:21:05 +0800

    Initial commit

---
+++ app/config/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package contains the configuration for each module in this project.
+"""
+
+from . import (
+    path,
+    logger
+)
---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+This optional file is the interface to run the whole project.
+"""
+
+import app
+
+if __name__ == '__main__':
+    pass
